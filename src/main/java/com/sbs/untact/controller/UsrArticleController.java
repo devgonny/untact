b1750873da9730236caccaff88833c404fb347fd
gonveloper <dgryu92@gmail.com>
2022-01-27 13:43:46 +0900

    게시물 삭제

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -15,41 +15,68 @@ import com.sbs.untact.dto.Article;
 public class UsrArticleController {
 	private List<Article> articles;
 	private int articlesLastId;
-	
+
 	public UsrArticleController() {
-		//멤벼변수 초기화
+		// 멤벼변수 초기화
 		articlesLastId = 0;
 		articles = new ArrayList<>();
-		
+		//게시물 2개생성
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목2", "내용2"));
-		
+
 	}
-	
-	
+
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
 		return articles.get(id - 1);
 	}
-		
 
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
 	}
-	
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Map<String, Object> doAdd(String regDate, String title, String body) {
 		articles.add(new Article(++articlesLastId, regDate, title, body));
-		
+
 		Map<String, Object> rs = new HashMap<>();
 		rs.put("resultCode", "S-1");
 		rs.put("msg", "성공하였습니다.");
 		rs.put("id", articlesLastId);
-		
+
 		return rs;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public Map<String, Object> doDelete(int id) {
+		boolean deleteArticleRs = deleteArticle(id);
+
+		Map<String, Object> rs = new HashMap<>();
+
+		if (deleteArticleRs) {
+			rs.put("resultCode", "S-1");
+			rs.put("msg", "성공하였습니다..");
+		} else {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", "해당게시물은 존재하지 않습니다.");
+		}
+		rs.put("id", id);
+
+		return rs;
+	}
+
+	private boolean deleteArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
 	}
 }
