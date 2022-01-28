6633811f32d1fbd8faeb0f27bc380a74e35424dd
dgryu92 <dgryu92@gmail.com>
2022-01-28 16:13:48 +0900

    Util.mapOf 적용

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -49,12 +49,14 @@ public class UsrArticleController {
 				
 		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
 
-		Map<String, Object> rs = new HashMap<>();
-		rs.put("resultCode", "S-1");
-		rs.put("msg", "성공하였습니다.");
-		rs.put("id", articlesLastId);
-
-		return rs;
+		/*
+		 * Map<String, Object> rs = new HashMap<>(); rs.put("resultCode", "S-1");
+		 * rs.put("msg", "성공하였습니다."); rs.put("id", articlesLastId);
+		 * 
+		 * return rs;
+		 */
+		// mapOf를 쓰면 아래 코드로 가능!		
+		return Util.mapOf("resultCode", "S-1", "msg", "성공하였습니다.", "id", articlesLastId);
 	}
 
 
@@ -65,16 +67,11 @@ public class UsrArticleController {
 
 		Map<String, Object> rs = new HashMap<>();
 
-		if (deleteArticleRs) {
-			rs.put("resultCode", "S-1");
-			rs.put("msg", "성공하였습니다..");
-		} else {
-			rs.put("resultCode", "F-1");
-			rs.put("msg", "해당게시물은 존재하지 않습니다.");
-		}
-		rs.put("id", id);
-
-		return rs;
+		if (deleteArticleRs == false) {
+			return Util.mapOf("resultCode", "F-1", "msg", "해당게시물은 존재하지 않습니다.");
+			
+		}  
+		return Util.mapOf("resultCode", "S-1", "msg", "성공하였습니다..", "id", id);
 	}
 
 	private boolean deleteArticle(int id) {
@@ -102,19 +99,13 @@ public class UsrArticleController {
 		Map<String, Object> rs = new HashMap<>();
 		
 		if (selArticle == null) {
-			rs.put("resultCode", "f-1");
-			rs.put("msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
-			return rs;
+			return Util.mapOf("resultCode", "f-1", "msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
 		} 
 		
 		selArticle.setUpdateDate(Util.getNowDateStr());
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
 		
-		rs.put("resultCode", "s-1");
-		rs.put("msg", String.format("%d번 게시물이 수정되었습니다.", id));
-		rs.put("id", id);
-
-		return rs;
+		return Util.mapOf("resultCode", "s-1", "msg", String.format("%d번 게시물이 수정되었습니다.", id),"id", id);
 	}
 }
