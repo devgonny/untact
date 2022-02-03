8c866bbe391888f354324a2287d921b9515820b3
dgryu92 <dgryu92@gmail.com>
2022-02-03 10:50:40 +0900

    관리자 페이지 추가 및 관리자 로그인

--- src/main/java/com/sbs/untact/service/MemberService.java
+++ src/main/java/com/sbs/untact/service/MemberService.java
@@ -37,9 +37,13 @@ public class MemberService {
 
 		return new ResultData("S-1", "회원정보가 수정되었습니다.");
 	}
-
+	
 	public boolean isAdmin(int actorId) {
-		return actorId == 1; //관리자 id지정
+		return actorId == 1;
+	}
+	
+	public boolean isAdmin(Member actor) {
+		return isAdmin(actor.getId());
 	}
 
 }
