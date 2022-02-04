f427370fb06e941ee160c7b5af467c0e2ce26f4e
dgryu92 <dgryu92@gmail.com>
2022-02-04 13:56:09 +0900

    외부로그인을 위해 회원테이블에 authKey 칼럼 추가

--- src/main/java/db.sql
+++ src/main/java/db.sql
@@ -1,7 +1,8 @@
 # 데이터베이스 생성
-DROP DATABASE IF EXISTS untactTeacher;
-CREATE DATABASE untactTeacher;
-USE untactTeacher;
+DROP DATABASE IF EXISTS untact;
+CREATE DATABASE untact;
+USE untact;
+
 # 게시물 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -10,22 +11,26 @@ CREATE TABLE article (
     title CHAR(100) NOT NULL,
     `body` TEXT NOT NULL
 );
+
 # 게시물, 테스트 데이터 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = "제목1 입니다.",
 `body` = "내용1 입니다.";
+
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = "제목2 입니다.",
 `body` = "내용2 입니다.";
+
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = "제목3 입니다.",
 `body` = "내용3 입니다.";
+
 # 회원 테이블 생성
 CREATE TABLE `member` (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -38,8 +43,10 @@ CREATE TABLE `member` (
     `email` CHAR(100) NOT NULL,
     `cellphoneNo` CHAR(20) NOT NULL
 );
+
 # 로그인 ID로 검색했을 때
 ALTER TABLE `member` ADD UNIQUE INDEX (`loginId`);
+
 # 회원, 테스트 데이터 생성
 INSERT INTO `member`
 SET regDate = NOW(),
@@ -50,9 +57,20 @@ loginPw = "user1",
 nickname = "user1",
 cellphoneNo = "01012341234",
 email = "jangka512@gmail.com";
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = "user2",
+loginPw = "user2",
+`name` = "user2",
+nickname = "user2",
+cellphoneNo = "01012341234",
+email = "jangka512@gmail.com";
+
 # 게시물 테이블에 회원번호 칼럼 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
-SELECT * FROM article;
+
 # 기존 게시물의 작성자를 회원1로 지정
 UPDATE article
 SET memberId = 1
@@ -65,4 +83,96 @@ SELECT NOW(), NOW(), FLOOR(RAND() * 2) + 1, CONCAT('제목_', FLOOR(RAND() * 100
 from article;
 */
 
-SELECT COUNT(*) FROM article;
+# 게시판 테이블 추가
+CREATE TABLE board (
+  id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  regDate DATETIME NOT NULL,
+  updateDate DATETIME NOT NULL,
+  `code` CHAR(20) UNIQUE NOT NULL,
+  `name` CHAR(20) UNIQUE NOT NULL
+);
+
+# 공지사항 게시판 추가
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+# 자유 게시판 추가
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'free',
+`name` = '자유';
+
+# 게시물 테이블에 게시판 번호 칼럼 추가, updateDate 칼럼 뒤에
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+# 기존 데이터를 랜덤하게 게시판 지정
+UPDATE article
+SET boardId = FLOOR(RAND() * 2) + 1
+WHERE boardId = 0;
+
+# 댓글 테이블 추가
+CREATE TABLE reply (
+  id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  regDate DATETIME NOT NULL,
+  updateDate DATETIME NOT NULL,
+  articleId INT(10) UNSIGNED NOT NULL,
+  memberId INT(10) UNSIGNED NOT NULL,
+  `body` TEXT NOT NULL
+);
+
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+articleId = 1,
+memberId = 1,
+`body` = "내용1 입니다.";
+
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+articleId = 1,
+memberId = 2,
+`body` = "내용2 입니다.";
+
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+articleId = 2,
+memberId = 2,
+`body` = "내용3 입니다."; 
+
+SELECT * FROM reply;
+
+# 게시물 전용 댓글에서 범용 댓글로 바꾸기 위해 relTypeCode 추가
+ALTER TABLE reply ADD COLUMN `relTypeCode` CHAR(20) NOT NULL AFTER updateDate;
+
+# 현재는 게시물 댓글 밖에 없기 때문에 모든 행의 relTypeCode 값을 article 로 지정
+UPDATE reply
+SET relTypeCode = 'article'
+WHERE relTypeCode = '';
+
+# articleId 칼럼명을 relId로 수정
+ALTER TABLE reply CHANGE `articleId` `relId` INT(10) UNSIGNED NOT NULL;
+
+# 고속 검색을 위해서 인덱스 걸기
+ALTER TABLE reply ADD KEY (relTypeCode, relId); 
+# SELECT * FROM reply WHERE relTypeCode = 'article' AND relId = 5; # O
+# SELECT * FROM reply WHERE relTypeCode = 'article'; # O
+# SELECT * FROM reply WHERE relId = 5 AND relTypeCode = 'article'; # X
+
+# authKey 칼럼을 추가
+ALTER TABLE `member` ADD COLUMN authKey CHAR(80) NOT NULL AFTER loginPw;
+
+# authKey 칼럼에 유니크 인덱스 추가
+ALTER TABLE `untact`.`member` ADD UNIQUE INDEX (`authKey`);
+
+# 기존 회원의 authKey 데이터 채우기
+UPDATE `member`
+SET authKey = CONCAT("authKey1__", UUID(), "__", RAND())
+WHERE authKey = '';
+
+SELECT * FROM `member`
