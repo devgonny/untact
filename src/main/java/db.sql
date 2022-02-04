dafcad8d67ea2e3346066cc33e700a0cd0ac9e33
dgryu92 <dgryu92@gmail.com>
2022-02-04 14:47:37 +0900

    authKey 파라미터가 입력되면, 로그인 없이도 글작성 가능하도록

--- src/main/java/db.sql
+++ src/main/java/db.sql
@@ -167,12 +167,14 @@ ALTER TABLE reply ADD KEY (relTypeCode, relId);
 # authKey 칼럼을 추가
 ALTER TABLE `member` ADD COLUMN authKey CHAR(80) NOT NULL AFTER loginPw;
 
-# authKey 칼럼에 유니크 인덱스 추가
-ALTER TABLE `untact`.`member` ADD UNIQUE INDEX (`authKey`);
-
 # 기존 회원의 authKey 데이터 채우기
 UPDATE `member`
-SET authKey = CONCAT("authKey1__", UUID(), "__", RAND())
-WHERE authKey = '';
+SET authKey = 'authKey1__1'
+WHERE id = 1;
 
-SELECT * FROM `member`
+UPDATE `member`
+SET authKey = 'authKey1__2'
+WHERE id = 2;
+
+# authKey 칼럼에 유니크 인덱스 추가
+ALTER TABLE `untact`.`member` ADD UNIQUE INDEX (`authKey`);
